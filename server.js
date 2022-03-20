bde0f1c667b89b8277c3e57139454e29b39b4399
franciscoatucha <franciscoatucha@gmail.com>
2022-03-20 20:34:45 -0300

    enter

--- server.js
+++ server.js
@@ -14,3 +14,16 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
+
+const server = http.createServer(function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/html' })
+	fs.readFile('index.html', function(error, data) {
+		if (error) {
+			res.writeHead(404)
+			res.write('Error: File Not Found')
+		} else {
+			res.write(data)
+		}
+	res.end()
+	})
+})
