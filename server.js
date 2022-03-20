e60c0e12a5843d6143c837cfac312b0f455a5cd2
franciscoatucha <franciscoatucha@gmail.com>
2022-03-20 20:30:00 -0300

    enter

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const app = express();
- 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Heroku atroden!!!')
-    .end();
-});
- 
-// Start the server
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+const http = require('http')
+const fs = require('fs')
+const port = 5001
+
+const server = http.createServer(function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/html' })
+	fs.readFile('index.html', function(error, data) {
+		if (error) {
+			res.writeHead(404)
+			res.write('Error: File Not Found')
+		} else {
+			res.write(data)
+		}
+	res.end()
+	})
+})
+
+server.listen(port, function(error) {
+	if (error) {
+		console.log('Something went wrong', error)
+		} else {
+		console.log('Server is listening to port' + port)
+		}
+})
