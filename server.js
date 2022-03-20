818f0b7312384622ccb8481cdd0ed3d16e74aaa3
franciscoatucha <franciscoatucha@gmail.com>
2022-03-20 19:58:04 -0300

    enter

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
  
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
